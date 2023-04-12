96232661452f5540d37c30fbcdd0986556f71ed1
upendra <ugrarapuupendra@gmail.com>
2023-04-12 11:29:24 +0530

    upd

--- main.tf
+++ main.tf
@@ -10,13 +10,15 @@ resource "aws_vpc" "main" {
 #public subnets
 
 resource "aws_subnet" "public_subnets" {
-  for_each = var.public_subnets
   vpc_id = aws_vpc.main.id
-  cidr_block = each.value["cidr_block"]
-  availability_zone = each.value["availability_zone"]
-
-  tags = merge(
+    tags = merge(
     var.tags, 
     {Name = "${var.env}-${each.value["name"]}"}
     )
+    
+  for_each = var.public_subnets 
+  cidr_block = each.value["cidr_block"]
+  availability_zone = each.value["availability_zone"]
+
+
 }
