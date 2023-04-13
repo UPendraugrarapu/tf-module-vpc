788d6100bbe91fc74c6c584ad990c7d93b0fb4c4
upendra <ugrarapuupendra@gmail.com>
2023-04-13 12:27:59 +0530

    upd

--- main.tf
+++ main.tf
@@ -30,6 +30,21 @@ resource "aws_internet_gateway" "igw" {
     {Name = "${var.env}-igw"}
     )
 }
+#NAT gateway
+resource "aws_eip" "nat" {
+      for_each = var.public_subnets
+      vpc      = true
+}
+resource "aws_nat_gateway" "nat-gateways" {
+  for_each = var.public_subnets
+  allocation_id = aws_eip[each.value["name"]].id 
+  subnet_id     = aws_subnet[each.value["name"]].id
+
+  tags = merge(
+    var.tags, 
+    {Name = "${var.env}-${each.value["name"]}"}
+    )
+}
 
 #public route table
 resource "aws_route_table" "public-route-table" {
