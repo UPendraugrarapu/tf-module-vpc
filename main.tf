07e5e7dff22a694ebaf623ac94acd730dbd82fac
upendra <ugrarapuupendra@gmail.com>
2023-04-13 13:37:25 +0530

    upd

--- main.tf
+++ main.tf
@@ -12,6 +12,10 @@ resource "aws_vpc_peering_connection" "peer" {
   peer_vpc_id   = var.default_vpc_id 
   vpc_id        = aws_vpc.main.id
   auto_accept   = true
+  tags = merge(
+    var.tags, 
+    {Name = "${var.env}-peer"}
+    )
 }
 
 #public subnets
@@ -61,6 +65,10 @@ resource "aws_route_table" "public-route-table" {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
   }
+  route {
+    cidr_block = data.aws_vpc.default_vpc.cidr_block
+    vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+  }
 
   for_each = var.public_subnets
   tags = merge(
@@ -102,6 +110,10 @@ resource "aws_route_table" "private-route-table" {
     cidr_block = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.nat-gateways["public-${split("-", each.value["name"])[1]}"].id
     #availabilty_zone taken reference from public subnets of natgw
+  }
+  route {
+    cidr_block = data.aws_vpc.default_vpc.cidr_block
+    vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
   }
    tags = merge(
     var.tags, 
